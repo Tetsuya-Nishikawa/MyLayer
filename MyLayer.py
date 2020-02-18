c4fee14dd03928192c0a4805417370a114f3c79e
Tetsuya-Nishikawa <61189782+Tetsuya-Nishikawa@users.noreply.github.com>
2020-02-18 21:10:30 +0900

    Add files via upload

---
+++ MyLayer.py
@@ -0,0 +1,30 @@
+#tensorflow(version2)で使用する自作Layerモジュール
+#論文：https://arxiv.org/pdf/1801.07455.pdfの(9)式を実装
+#self.Mは、(10)式以下に記載されている重みのこと
+
+class GcnLayer(tf.keras.layers.Layer):
+    def __init__(self, input_dimention,output_dimention):
+        super(GcnLayer, self).__init__()
+        self.output_dimention = output_dimention #出力次元
+        self.input_dimention = input_dimention      #入力次元
+        self.adjacency_matrix,self.degree_matrix  = tf.cast(tf.convert_to_tensor(construct_graph.MakeAdjacencyMatrix()), tf.float32) #隣接行列と次数行列
+        self.nodes = 49 #ノード数
+
+    def build(self, input_shape):
+
+        self.w = self.add_weight(shape=[self.input_dimention, self.output_dimention], initializer = tf.keras.initializers.he_normal(), trainable=True)
+        self.b = self.add_weight(shape=[self.output_dimention,], initializer = tf.keras.initializers.he_normal(), trainable=True)
+        self.M = tf.Variable(initial_value=tf.ones((self.nodes, self.nodes)), trainable=True)
+        
+    #https://www.tensorflow.org/guide/keras/masking_and_padding　の　”Supporting masking in your custom layers”　にcompute_maskの解説あり
+    def compute_mask(self, inputs, mask=None):
+        return mask
+    
+    #https://www.tensorflow.org/guide/keras/masking_and_padding　の　”Writing layers that need mask information”　に引数にmaskを指定する解説
+    def call(self, inputs, mask=None):
+        
+        A_ElementProduct_M =  tf.multiply(self.adjacency_matrix,self.M)
+        L = tf.matmul(self.degree_matrix, tf.matmul(A_ElementProduct_M, self.degree_matrix))
+        outputs = tf.matmul(L, tf.matmul(inputs,self.w))+self.b
+        
+        return tf.nn.relu(outputs)
